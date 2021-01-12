19df6c94c2a305737a6714b9872948b5e68e8cd7
Niklas Korz <korz.niklask@gmail.com>
2021-01-12 09:42:33 +0100

    Replace deprecated cgmath function

--- learn-wgpu/src/camera.rs
+++ learn-wgpu/src/camera.rs
@@ -20,7 +20,7 @@ pub struct Camera {
 
 impl Camera {
     pub fn build_view_projection_matrix(&self) -> cgmath::Matrix4<f32> {
-        let view = cgmath::Matrix4::look_at(self.eye, self.target, self.up);
+        let view = cgmath::Matrix4::look_at_rh(self.eye, self.target, self.up);
         let proj = cgmath::perspective(cgmath::Deg(self.fovy), self.aspect, self.znear, self.zfar);
         return OPENGL_TO_WGPU_MATRIX * proj * view;
     }
